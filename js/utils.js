21d805363836fd39366f2f5da2aa4982b6bcd748
Mdmohdirfan111 <133225540+Mdmohdirfan111@users.noreply.github.com>
2026-08-14 23:32:38 +0530

    Delete js directory

--- js/utils.js
+++
@@ -1,8 +0,0 @@
-function showMessage(msg, type) {
-    const el = document.getElementById('message');
-    if (!el) return;
-    el.textContent = msg;
-    el.className = 'message-box show ' + type;
-    setTimeout(() => el.className = 'message-box', 4000);
-}
-window.showMessage = showMessage;
